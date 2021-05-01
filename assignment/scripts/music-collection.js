a82d9cad4cf1e935b43f15d8266e81ad4e74b70c
Stephen Piper <stephen.r.piper@gmail.com>
2021-04-30 22:59:47 -0500

    Created addToCollection function.  Currently creates obj newAlbum and adds it to the end of Collection array.

--- assignment/scripts/music-collection.js
+++ assignment/scripts/music-collection.js
@@ -1,3 +1,17 @@
 console.log('***** Music Collection *****')
 
 let collection = []; // creates array variable collection
+
+function addToCollection(title, artist, yearPublished,) { //start addToCollection function.
+  console.log('In addToCollection');
+  let newAlbum = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished,
+  }//creates new object and grabs inputs
+  collection.push(newAlbum);//adds newly made object to the end of collection array
+
+  return newAlbum;//returns the entered info
+}//end addToCollection function
+console.log(addToCollection('Faces in Relfection', 'George Duke','1974'));
+console.log('My albums:', collection);//Logs collections array
